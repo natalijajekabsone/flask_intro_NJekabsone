2772d82ae9f070b344a6da3ca1e0b2172948e09d
[Students] Natalija Jekabsone <93DPNJekabsone@rvt.lv>
2020-10-16 15:09:36 +0300

    k

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Sveika pasaule<h1>"
+    return render_template("home.html")
+@app.route('/about')
+def about():
+    return render_template("about.html")
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
 
 app.run(host="0.0.0.0", port=80, debug=True)
