50a449ddb641ed4db51abb90f513d01390022f22
Natasha Jekabsone <natashajekabsone@MacBook-Air-Nata.local>
2020-10-23 13:55:56 +0300

    fewwf

--- main.py
+++ main.py
@@ -1,46 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template("index.html")
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    return render_template()
 
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')        
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
-@app.route('/getParam', methods = ['GET'])
-def getParam():
-    #print(request.args)
-    return f"It works {request.args.get('vards')}"
-
-@app.route('/postMsg', methods = ['POST'])
-def postMsg():
-    dati = request.json
-
-    return f"This is postMsg! {dati['vards']}"
-
-@app.route('/info', methods = ['GET', 'POST'])
-def info():
-    if request.method == 'GET':
-        if request.args.get('vards'):
-            return f"Hello, {request.args.get('vards')}!"
-        else:
-            return "Hello, stranger!"
-    elif request.method == "POST":
-        if request.content_type == 'application/json':
-            dati = request.json
-            return dati
-        else:
-            return f"Wrong content type {request.content_type}"
-    else:
-        return "This unknow method!"
-
-
-
-app.run(host="0.0.0.0", port=80, debug=True)
+if __name__ == "__main__":
+app.run(debug=True)
